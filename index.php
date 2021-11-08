915f3d85953e4ef0f622a1bbcd3c29c49a1f7954
Kemal ŞİMŞEK <kemsoft@gmail.com>
2021-11-08 10:58:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,269 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="tr" lang="tr" dir="ltr" >
+<head>
+ <title>AKILLI ÖĞRENME SİSTEMİ (ILS)</title>    
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 
+ <meta name=”description” content=”Akıllı Öğrenme Sistemi, Intelligent Learning Systems, Matematik ve Bilgisayar Akademisi, Maths and Computer Academy, Matematik, Mathematics, Bilgisayar, Computer, Education, Eğitim, Matematik Eğitimi, Harmanlanmış Öğrenme, Blended Learning, Hybrit Learning, Karma Öğrenme, Bilgisayar Cebiri Sistemleri, Computer Algebra Systems, Remote Learning, Uzaktan Öğrenme, Online Öğrenme”/>
+ <meta content='width=device-width, initial-scale=1.0' name='viewport' />
+   
+    <link rel="stylesheet" type="text/css" href="css/tasarim.css" />   
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/yerlesim.css" />
+    <link rel="stylesheet" type="text/css" href="css/ekran.css" media="screen and (min-width: 768px)" />
+    <link rel="stylesheet" type="text/css" href="css/tasarim.css" media="screen and (min-width: 0px) and (max-width: 600px)" />
+	
+	
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="js/global.js"></script>
+	<script type="text/javascript">
+                var ga_userId = 0;  </script>
+    <style type="text/css">
+<!--
+.style3 {color: #FF0000}
+-->
+    </style>
+</head>
+
+<body itemscope itemtype="http://schema.org/WebPage" class="ltr">
+    
+
+    	
+	
+		
+		
+			
+					
+                  
+	
+    <div class="ggbPageContent">
+      <h1 align="center" class="caps "><strong>MATEMATİK &amp; BİLGİSAYAR AKADEMİSİ </strong></h1>
+      <div>  <center> <a href="index.php"></a></center>
+                <?php require("menu.php"); ?></div>
+      <div class="imageButtonContainer resize regular clearfix">
+      <a class="imageButton" title="Materiyalleri Gezin" href="aos.php">
+            <img src="img/bilgCom.gif" alt="" width="225" height="210" border="0" />
+      <h2 align="left"><strong>AKILLI ÖĞRENME SİSTEMİ</strong></h2>
+        </a>
+      <a class="imageButton" title="Olu&#351;turmaya ba&#351;la" href="etkinlikler.php" target="_blank">
+            <img src="img/PuzzlePiece2.jpg" alt="" width="230" height="135" />
+      <h2 align="left"><strong>ÖĞRENME ARAÇLARI</strong></h2>
+      </a>
+      <a class="imageButton" title="Materiyalleri Gezin" href="mathcom.php">
+            <img src="img/AlwaysAvailable.png" alt="" width="224" height="192" border="0" />
+      <h2 align="left"><strong>MATEMATİK &amp; BİLGİSAYAR</strong></h2>
+        </a>
+      <p>&nbsp;</p>
+      
+      </div>
+      
+      
+      
+
+    </div>
+    <div class="loveBanners">
+    <div id="studentsLoveBanner" class="ggbBanner green">
+        <div class="ggbBannerContent">
+            <div class="box">
+                <p class="big">
+                    
+                   Sembolik İşlemleri Destekler              </p>
+            </div>
+            <div id="studentsLove" class="slide">
+                <div class="visible flex box left clearfix">
+                   
+                    <h3> Sembolik İşlemleri Destekler </h3>
+                    <p>Yaklaşık değerler için sembol ya da sayısal değerleri yerine koyma <br>
+		İfade biçimlerini değiştirmek<br>
+		 Genişleyen çarpımlar ve kuvvetler, 
+		 <br>Kısmi ve tam çarpanlara ayırma, <br> Kısmi kesirin yeniden yazılması, <br>kısıtlı ödeme,                  </p>
+                </div>
+                <div class="box right flex clearfix">
+                    
+                    <h3>
+                         Sembolik İşlemleri Destekler                  </h3>
+                    <p>                       
+					    trigonometrik fonksiyonların üstel olarak yazılması,
+						<br> mantıksal ifadelerin dönüşümü, vs.
+						<br>Kısmi ve tam türevler
+						<br>Çok boyutlu integraller dahil, bazı belirsiz ve belirli integral alma
+					   	   
+                  </p>
+                </div>
+                <div class="box left flex clearfix">
+                    <img src="img/AlwaysAvailable.png" alt="" />
+                    <h3> Sembolik İşlemleri Destekler </h3>
+                    <p>
+						Sembolik kısıtlı ve kısıtlı olmayan küresel uyumluluk
+						<br>Çeşitli etki alanları üstünde doğrusal ve doğrusal olmayan denklemlerin çözümü
+						<br>Bazı diferansiyel ve fark denklemlerinin çözümü
+						<br> Bazı limitleri almak
+						<br> İntegral dönüşümleri
+						
+                  </p>
+                </div>
+                <div class="box right flex clearfix">
+                    
+                    <h3>Sembolik İşlemleri Destekler</h3>
+                    <p>
+                       Seri operasyonlarının toplam ve çarpımlar şeklinde açılımı,
+					   <br> Matris operasyonlarını içeren çarpımlar, ters almalar, vs.
+					   <br> İstatistiksel hesaplamalar
+					   <br> Deneysel matematik bölgesi içinde teorem ispatlama ve doğrulama çok kullanışlıdır.
+			      </p>
+                </div>
+
+                <div class="clear"></div>
+
+                <a class="navigate prev">
+                    <span class="rectangle"></span>
+                    <span class="img"></span>
+                </a>
+                <a class="navigate next">
+                    <span class="rectangle"></span>
+                    <span class="img"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div id="teachersLoveBanner" class="ggbBanner orange">
+        <div class="ggbBannerContent">
+            <div class="box">
+                <p class="big">
+                    
+                    Ek yetenekler               </p>
+            </div>
+            <div id="teachersLove" class="slide">
+                <div class="visible flex box right clearfix">
+                    
+                    <h3>  Bir Programlama dili, kullanıcıların kendi algoritmalarını uygulamalarına izin verir...</h3>
+                    <p>
+                        
+						İsteğe bağlı-Hassas Sayısal İşlemler
+						<br>
+						Kesin tamsayı aritmetiği ve Sayı kuramı işlevselliği
+						<br>
+Matematiksel ifadeleri iki-boyutlu matematiksel-formda göstermek,
+<br>çoğu kez TeXe benzer damga dizme sistemleri (ayrıca bakınız Prettyprint)
+                  </p>
+                </div>
+                <div class="box flex left clearfix">
+                    
+                    <h3>
+                        Bir Programlama dili, kullanıcıların kendi algoritmalarını uygulamalarına izin verir...                 </h3>
+                    <p>					   
+					   çizim grafikler ve parametrik çizimler fonksiyonların iki ve üç boyutta,<br>
+					    ve onları anime etme 
+						<br>Grafikler ve diyagramlar çizme
+						<br> API'ler için onu bir dış program üstünde bir veritabanı gibi bağlama,
+						<br> ya da bir programlama dilinde kullanıp bilgisayarlı cebir sistemi kullanmak
+                  </p>
+                </div>
+                <div class="box flex right clearfix">
+                    
+                    <h3>Bir Programlama dili, kullanıcıların kendi algoritmalarını uygulamalarına izin verir...</h3>
+                    <p>
+                        
+						dizi işlemleri gibi eşleştirme ve arama
+						<br> matematik uygulamalarında örneğin Fizik, 
+						Biyoinformatik, hesaplamalı kimya'daki gibi eklenti için kullanma ve fiziksel hesaplama için paketlemek
+						
+                  </p>
+                </div>
+
+                <div class="clear"></div>
+
+                <a class="navigate prev">
+                    <span class="rectangle"></span>
+                    <span class="img"></span>
+                </a>
+                <a class="navigate next">
+                    <span class="rectangle"></span>
+                    <span class="img"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div id="schoolsLoveBanner" class="ggbBanner red">
+        <div class="ggbBannerContent">
+            <div class="box">
+                <p class="big">
+                   
+                    Matematiği Somutlaştırır..           </p>
+                <br/>
+            </div>
+            <div class="box flex center">
+                <p class="borderBox">
+                    BCS kullanan ö&#287;renciler                </p>
+                <p class="minor huge" style="line-height: 50%">=</p>
+                <p class="borderBox">
+                    Daha iyi motive olmu&#351; ö&#287;renciler                </p>
+                <p class="minor huge" style="line-height: 50%">=</p>
+                <p class="borderBox">
+                    Daha iyi not alan ö&#287;renciler                </p>
+                <br/>
+            </div>
+        </div>
+    </div>
+</div>
+        <div id="ggbFooterPlaceholder"></div>
+
+</div>
+
+
+
+<div id="ggbFooter" align="center">
+    <div id="ggbFooterTop">
+        <div id="ggbFooterTopContent">
+            <ul id="ggbFooterMenu">
+                            <li>              </li>
+                            <li>
+                              <ul>
+                                <li> <a href='matsonuc.php'>Sınav Sonuçları </a></li>
+                                <li></li>
+                              </ul>
+              </li>
+                    <li>
+                      <ul>
+                                                                <li>
+                <a href='https://drive.google.com/file/d/0B5l0nanD3OygQkRCLXpONDJfWm8/view?usp=sharing'>7.Sınıf Çalışma Soruları </a>                            </li>
+                        <li></li>
+                   </ul>
+              </li>
+                    <li>
+                      <ul>
+                       <li>
+                <a href='yurtici.php'>Yurtiçi Çalışmalar </a>        </li>
+                        <li></li>
+                      </ul>
+              </li>
+                        <li>
+                          <ul>
+                        <li><a href="iletisim.php">İletişim &amp; İletişim Formu </a></li>
+                        </ul>
+              </li>
+          </ul>
+            <div id="ggbFooterOptions">
+                <div id="ggbFollowUs"></div>
+          </div>
+      </div>
+        <div class="clear"></div>
+    </div>
+
+    <div id="ggbFooterBottom">
+      <div id="ggbFooterBottomContent">
+            <ul>
+                            <li></li>
+                            <li>
+                <a href='biz.php'>Hakkımızda! Biz kimiz? </a>              </li>
+                            <li></li>
+        </ul>
+          &copy; 2015 Kemal ŞİMŞEK       &copy;</div>
+    </div>
+</div>
+<!-- orta kısım sonu -->
+</body>
+</html>
